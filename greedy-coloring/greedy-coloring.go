3fc3b4a0068b6d63e5dab9deab62868b4bd2ca9c
Fedor <ffedorovzx@gmail.com>
2026-03-18 03:01:33 +0300

    added: greedy coloring and tree diameter

--- greedy-coloring/greedy-coloring.go
+++ greedy-coloring/greedy-coloring.go
@@ -1,5 +1,61 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func main() {
-	// TODO: implement
+	var m, n int
+	fmt.Scan(&m, &n)
+
+	graph := make(map[int][]int, n+1)
+	indegree := make([]int, n+1)
+
+	for i := 0; i < m; i++ {
+		var u, v int
+		fmt.Scan(&u, &v)
+
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
+		indegree[u] = indegree[u] + 1
+		indegree[v] = indegree[v] + 1
+	}
+
+	nodes := make([]int, n)
+	for i := 0; i < n; i++ {
+		nodes[i] = i + 1
+	}
+
+	sort.Slice(nodes, func(i, j int) bool {
+		return indegree[nodes[i]] > indegree[nodes[j]]
+	})
+
+	colors := make([]int, n+1)
+	for i := range colors {
+		colors[i] = -1
+	}
+
+	notAvaliable := make([]bool, n+1)
+
+	colors[1] = 1
+
+	for _, i := range nodes {
+		for _, u := range graph[i] {
+			if colors[u] != -1 {
+				notAvaliable[colors[u]] = true
+			}
+		}
+		for j := 1; j < n+1; j++ {
+			if !notAvaliable[j] {
+				colors[i] = j
+				break
+			}
+		}
+		for _, u := range graph[i] {
+			if colors[u] != -1 {
+				notAvaliable[colors[u]] = false
+			}
+		}
+	}
 }
--- tree-diameter/tree-diameter.go
+++ tree-diameter/tree-diameter.go
@@ -1,5 +1,75 @@
 package main
 
+import (
+	"algorithms/structures"
+	"fmt"
+	"slices"
+)
+
 func main() {
-	// TODO: implement
+	var m, n int
+	fmt.Scan(&m, &n)
+
+	graph := make(map[int][]int, n+1)
+
+	for i := 0; i < m; i++ {
+		var u, v int
+		fmt.Scan(&u, &v)
+
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
+	}
+
+	visited := make([]bool, n+1)
+	visited[1] = true
+
+	dist := make([]int, n+1)
+	dist[1] = 0
+
+	q := structures.NewQueue[int]()
+	q.Enqueue(1)
+
+	maxID := 1
+
+	for q.Size() > 0 {
+		s, _ := q.Peek()
+		q.Dequeue()
+
+		for _, v := range graph[s] {
+			if visited[v] {
+				continue
+			}
+			dist[v] = dist[s] + 1
+			if dist[v] > dist[maxID] {
+				maxID = v
+			}
+			visited[v] = true
+			q.Enqueue(v)
+		}
+	}
+
+	visited = make([]bool, n+1)
+	visited[maxID] = true
+
+	dist = make([]int, n+1)
+	dist[maxID] = 0
+
+	q = structures.NewQueue[int]()
+	q.Enqueue(maxID)
+
+	for q.Size() > 0 {
+		s, _ := q.Peek()
+		q.Dequeue()
+
+		for _, v := range graph[s] {
+			if visited[v] {
+				continue
+			}
+			dist[v] = dist[s] + 1
+			visited[v] = true
+			q.Enqueue(v)
+		}
+	}
+
+	fmt.Println(slices.Max(dist))
 }
